f6803cead48f52433a626cc2920b21c241f2a415
Marin Andreev <48124400+marinandreew@users.noreply.github.com>
2022-02-11 08:43:43 +0200

    created home, login/reg, reservation-crud and manage + db

---
+++ counters/pendingpayment.php
@@ -0,0 +1,10 @@
+<?php 
+    include './db.php';
+    $sql = "SELECT SUM( total_price) FROM booking WHERE payment_status = '0'";
+    $amountsum = mysqli_query($connection, $sql) or die(mysqli_error($sql));
+        $row_amountsum = mysqli_fetch_assoc($amountsum);
+        $totalRows_amountsum = mysqli_num_rows($amountsum);
+    echo $row_amountsum['SUM( total_price)'];
+
+
+?>
